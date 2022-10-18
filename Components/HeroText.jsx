01308204e49825d46b16ff726d76fead9103b459
David Aslan <davidaslan112671@live.se>
2022-10-18 16:09:43 +0200

    4pm

--- Components/HeroText.jsx
+++ Components/HeroText.jsx
@@ -3,11 +3,12 @@ export default class HeroText extends Component {
   heroWords = ["Family", "Friends", "Work", "Fun"];
   render() {
     return (
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-80">
         <div className="text-center flex">
-          <span className="text-3xl font-thin">TOGETHER.</span>
-          <span className="text-3xl font-bold">TOGETHER.</span>
+          <span className="text-7xl font-thin">PIONEERS.</span>
+          <span className="text-7xl font-bold">TOGETHER.</span>
         </div>
+        
       </div>
     );
   }
