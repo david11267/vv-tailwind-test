2d532d0cefc172c131b0066520f9af2dd105b4fc
David Aslan <davidaslan112671@live.se>
2022-10-19 21:24:15 +0200

    e

--- Components/ExplainingText.jsx
+++ Components/ExplainingText.jsx
@@ -9,7 +9,7 @@ export default class ExplainingText extends Component {
           <div className="w-3/4">
             <h1>VARMEVERKET IS A CREATIVE SPACE THAT WORKS WITH PIONEER PROJECTS WHICH ARE POSITIVE TO PEOPLE.</h1>
             <h1 className=" mt-5 mb-5 md:m-5">
-              ARE YOU A FUTURE.<span className="font-bold">PIONEER?</span>
+              ARE YOU A FUTUREe.<span className="font-bold">PIONEER?</span>
             </h1>
             <div className="flex justify-center">
               <CallToActionLink text="APPLY NOW!" />
