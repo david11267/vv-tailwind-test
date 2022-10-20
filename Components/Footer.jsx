7328415e1abcb4ed7abae91fcf5a02cb421c9eb4
David Aslan <davidaslan112671@live.se>
2022-10-20 11:43:30 +0200

    lunch

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,40 +3,43 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer className=''>
-        <div className=' bg-white text-black'>
-            <p className='font-bold'>Contact</p>
-            <div className='flex mt-8 mb-8'>
-            <h2 className='text-6xl'>Keep in touch</h2>
-            <img className='filter invert w-20 ml-2' src="https://hypermedia.varmeverket.com/logo.svg" alt="logo" />
-            </div>
-            
-            <p className='text-sm'>Start a conversation</p>
-            <a className='underline text-3xl' href="mailto:info@varmeverket.com">
-                <p>Info@varmeverket.com</p>
-            </a>
-            <div className='flex flex-row'>
-                <div className='basis-1/2'>
-                    <p className='text-sm font-bold'>Our office</p>
-                    <p className='text-4xl'>Stockholm</p>
-                    <p >Bredängsvägen 203
-                    127 34 Skärholmen
-                    Stockholm</p>
-                    <p>Our policy</p>
-                    <p>Terms of service</p>
+      <footer className=' text-black bg-white flex justify-center'>
+        <div className='mt-8 w-2/4'>
+          <div>
+            <p className='mb-4' >Contact</p>
+            <h1>
+              <span>KEEP.IN.</span><span>TOUCH.</span>
+            </h1>
+            <p className='mb-4'>Start a conversation</p>
+            <h1 className='mb-4'>
+              <a href="mailto:info@varmeverket.com">info@varmeverket.com</a>
+            </h1>
+
+            <div className="flex">
+              <div className="mr-40">
+                <h2 className="text-2xl mb-4" >Our office</h2>
+                <h1>Stockholm</h1>
+                <p className="mb-4">
+                  Bredängsvägen 203 <br />
+                  12734 Skärholmen <br />
+                  Stockholm <br />
+                </p>
+                <h3 className="text-xl mb-4">OUR POLICY</h3>
+                <a className='underline font-semibold'>TERMS OF SERVICE</a>
+              </div>
+
+              <div>
+                <div className="align-middle">
+                  <h2 className="text-2xl mb-4" >Our Socials</h2>
+                  <a className="underline text-2xl">LinkedIn</a>
+                  <br />
+                  <a className="underline text-2xl">Instagram</a>
                 </div>
-                <div className='basis-1/2'> 
-                    <p className='text-sm font-bold'>Our socials</p>
-                    <a className='underline text-3xl' href='https://www.linkedin.com/company/varmeverket/about/'>
-                        <p>LinkedIn</p>
-                    </a>
-                    <a className='underline text-3xl' href='https://www.instagram.com/varmeverket/'>
-                        <p>
-                           Instagram 
-                        </p>
-                    </a>
-                </div>
+              </div>
+
+
             </div>
+          </div>
         </div>
       </footer>
     )
