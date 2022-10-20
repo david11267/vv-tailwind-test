9d4c675e0cf5b5abbea06583c409c898aa2aff2e
David Aslan <davidaslan112671@live.se>
2022-10-20 22:53:22 +0200

    fukkk

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        GTAmericaExpandedRegular: ["GT America Expanded", "sans-serif"],
-        GTAmericaExpandedBlack: ["GT America Expanded Black", "sans-serif"],
-        FuzzyBubble: ["Fuzzy Bubbles", "cursive"],
-        GtAmericaCompressed: ["GT America Compressed", "sans-serif"],
-      },
+    fontFamily: {
+      GTAmericaExpandedRegular: ["GT America Expanded", "sans-serif"],
+      GTAmericaExpandedBlack: ["sam", "sans-serif"],
+      FuzzyBubble: ["Fuzzy Bubbles", "cursive"],
+      GtAmericaCompressed: ["GT America Compressed", "sans-serif"],
     },
+    extend: {},
   },
   plugins: [],
 };
