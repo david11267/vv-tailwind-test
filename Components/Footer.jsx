77defaf252becd9862e44bba3ce0e095c20c5147
David Aslan <davidaslan112671@live.se>
2022-10-27 15:58:43 +0200

    forms draft2

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,37 +3,39 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer className=' text-black bg-white flex justify-center'>
-        <div className='mt-8 w-2/4'>
+      <footer className=' text-black bg-white '>
+        <div className='mt-8 ml-20'>
           <div>
-            <p id="contact" className='mb-4' >Contact</p>
-            <h1>
-              <span>KEEP.IN.</span><span>TOUCH.</span>
+            <p id="contact" className='mb-4 ' >Contact</p>
+            <h1 className="text-9xl mb-12">
+              <span>KEEP.IN.</span><span className="font-bold">TOUCH.</span>
             </h1>
-            <p className='mb-4'>Start a conversation</p>
-            <h1 className='mb-4'>
+            <p className='mb-12 text-4xl'>Start a conversation</p>
+            <h1 className='mb-12 text-7xl'>
               <a href="mailto:info@varmeverket.com">info@varmeverket.com</a>
             </h1>
 
-            <div className="flex">
+            <div className="flex pb-12">
               <div className="mr-40">
-                <h2 className="text-2xl mb-4" >Our office</h2>
-                <h1>Stockholm</h1>
-                <p className="mb-4">
-                  Bredängsvägen 203 <br />
-                  12734 Skärholmen <br />
-                  Stockholm <br />
+                <h2 className="text-5xl mb-4" >Our office</h2>
+                <h1 className="text-7xl mb-4">Stockholm</h1>
+                <p className="mb-4 text-4xl">
+                  Bredängsvägen 203
                 </p>
-                <h3 className="text-xl mb-4">OUR POLICY</h3>
-                <a className='underline font-semibold'>TERMS OF SERVICE</a>
+                <p className="mb-4 text-4xl">
+                  12734 Skärholmen
+                </p>
+                <p className="mb-12 text-4xl"> Stockholm </p>
+                <h3 className="text-3xl mb-4 font-GtAmericaMono">OUR POLICY</h3>
+                <a className='underline text-4xl font-semibold'>TERMS OF SERVICE</a>
               </div>
 
               <div>
-                <div className="align-middle">
-                  <h2 className="text-2xl mb-4" >Our Socials</h2>
-                  <a className="underline text-2xl">LinkedIn</a>
+                <div className="mt-20">
+                  <h2 className="text-5xl mb-20" >Our Socials</h2>
+                  <a className="underline text-5xl">LinkedIn</a>
                   <br />
-                  <a className="underline text-2xl">Instagram</a>
+                  <a className="underline text-5xl">Instagram</a>
                 </div>
               </div>
 
