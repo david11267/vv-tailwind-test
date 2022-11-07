b5b81270b12c1fab755257ac252e19dd9fb8f143
David Aslan <davidaslan112671@live.se>
2022-11-07 15:32:51 +0100

    working getSpaces

--- Components/Modal.jsx
+++ Components/Modal.jsx
@@ -1,33 +1,43 @@
-import { useState } from "react";
+import { Result } from "postcss";
+import { useState, useEffect } from "react";
 import { useModalContext } from "./ModalContextProvider";
-
-const spaces = [
-  {
-    id: "panncentralen",
-    label: "Panncentralen",
-  },
-  {
-    id: "musikverket",
-    label: "Musikverket",
-  },
-];
-
 
 
 const Modal = () => {
+  const [spaces, setSpaces] = useState([]);
   const [formValues, setFormValues] = useState({
-    space: "",
-    date: "",
+    space_name: "",
+    start: "",
+    end: "",
+    user_email: "",
     additionalInfo: "",
   });
+
+
+
+  useEffect(() => {
+    getSpaces();
+  }, []);
+
+  async function getSpaces() {
+    await fetch("https://api.varmeverket.com/v2/spaces", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        setSpaces(data);
+        return data;
+      });
+  }
+
   const { setIsOpen } = useModalContext();
 
   const onSubmit = (event) => {
     event.preventDefault();
     console.log(formValues);
-    const response = fetch("https://api.varmeverket.com/v3/forms/booking", {
+    const response = fetch("https://api.varmeverket.com/v2/bookings", {
       body: JSON.stringify(formValues),
-      headers: {},
+      headers: {
+        contentType: "application/json",
+      },
       method: "POST",
     });
     setIsOpen(false);
@@ -41,7 +51,11 @@ const Modal = () => {
 
   };
 
+  const [modalFace, setModalFace] = useState();
+
+
   return (
+
     <form
       onSubmit={onSubmit}
       className="relative flex flex-col m-auto max-w-5xl w-[90vw] mb-8 p-8 bg-gray-600 bg-opacity-80"
@@ -55,38 +69,67 @@ const Modal = () => {
         >
           X
         </button>
-      </div>
+      </div >
       <div className="grid gap-4">
         <div className="grid gap-2">
-          <label className="text-white" htmlFor="space">
+          <label className="text-white" htmlFor="space_name">
             SELECT THE SPACE
           </label>
           <select
             className="w-full p-4 bg-black bg-opacity-80"
             style={{ borderRight: "16px solid transparent" }}
-            name="space"
-            id="space"
+            name="space_name"
+            id="space_name"
             onChange={handleChange}
           >
             <option className="hidden"></option>
             {spaces.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.label}
+              <option key={spaces.name} value={item.name}>
+                {item.name} {item.description}
               </option>
             ))}
           </select>
         </div>
+        <div className="flex">
+          <div className="grid gap-2 mr-4">
+            <label className="text-white" htmlFor="start">
+              CHOOSE START DATE
+            </label>
+            <input
+              style={{ colorScheme: "dark" }}
+              onChange={handleChange}
+              name="start"
+              id="start"
+              className="p-4 bg-black bg-opacity-80"
+              type="datetime-local"
+            />
+          </div>
+          <div className="grid gap-2 mr-4">
+            <label className="text-white" htmlFor="end">
+              CHOOSE END DATE
+            </label>
+            <input
+              style={{ colorScheme: "dark" }}
+              onChange={handleChange}
+              name="end"
+              id="end"
+              className="p-4 bg-black bg-opacity-80"
+              type="datetime-local"
+            />
+          </div>
+
+        </div>
         <div className="grid gap-2">
-          <label className="text-white" htmlFor="date">
-            CHOOSE A DATE
+          <label className="text-white" htmlFor="user_email">
+            WRITE YOUR EMAIL
           </label>
           <input
             style={{ colorScheme: "dark" }}
             onChange={handleChange}
-            name="date"
-            id="date"
+            name="user_email"
+            id="user_email"
             className="p-4 bg-black bg-opacity-80"
-            type="datetime-local"
+            type="email"
           />
         </div>
         <div className="grid gap-2">
@@ -106,7 +149,7 @@ const Modal = () => {
       <button className="bg-gray-400 p-2 mt-4 ml-auto" type="submit">
         SEND REQUEST
       </button>
-    </form>
+    </form >
   );
 };
 
